178c47ac0807e6a869b513a1f644e0c46d8f3fe3
Ravi Mendis <rmendis@mac.com>
2020-12-16 09:51:45 +0000

    Basic support for GS
    GS map added with first iteration of polar ice generation.  (Basic)

---
+++ Maps/Greenland_XP2.lua
@@ -0,0 +1,588 @@
+-- Greenland.lua
+-- Author: blkbutterfly74
+-- DateCreated: 16/12/2020
+-- Creates a Standard map shaped like real-world Greenland 
+-- based off Scrambled Australia map script
+-- Thanks to Firaxis
+-----------------------------------------------------------------------------
+
+include "MapEnums"
+include "MapUtilities"
+include "MountainsCliffs"
+include "RiversLakes"
+include "FeatureGenerator"
+include "TerrainGenerator"
+include "NaturalWonderGenerator"
+include "ResourceGenerator"
+include "CoastalLowlands"
+include "AssignStartingPlots"
+
+local g_iW, g_iH;
+local g_iFlags = {};
+local g_continentsFrac = nil;
+local g_iNumTotalLandTiles = 0; 
+local g_CenterX = 18;
+local g_CenterY = 35;
+local featureGen = nil;
+
+-------------------------------------------------------------------------------
+function GenerateMap()
+	print("Generating Greenland Map");
+	local pPlot;
+
+	-- Set globals
+	g_iW, g_iH = Map.GetGridSize();
+	g_iFlags = TerrainBuilder.GetFractalFlags();
+	local temperature = 0;
+
+	--	local world_age
+	local world_age_new = 5;
+	local world_age_normal = 3;
+	local world_age_old = 2;
+
+	local world_age = MapConfiguration.GetValue("world_age");
+	if (world_age == 1) then
+		world_age = world_age_new;
+	elseif (world_age == 3) then
+		world_age = world_age_old;
+	else
+		world_age = world_age_normal;	-- default
+	end
+	
+	plotTypes = GeneratePlotTypes(world_age);
+	terrainTypes = GenerateTerrainTypesGreenland(plotTypes, g_iW, g_iH, g_iFlags, true);
+	ApplyBaseTerrain(plotTypes, terrainTypes, g_iW, g_iH);
+
+	AreaBuilder.Recalculate();
+	--[[ blackbutterfly74 - Why this additional AnalyzeChockepoint()? Commenting out for now:
+	TerrainBuilder.AnalyzeChokepoints(); --]]
+	TerrainBuilder.StampContinents();
+
+	local iContinentBoundaryPlots = GetContinentBoundaryPlotCount(g_iW, g_iH);
+	local biggest_area = Areas.FindBiggestArea(false);
+	print("After Adding Hills: ", biggest_area:GetPlotCount());
+	AddTerrainFromContinents(plotTypes, terrainTypes, world_age, g_iW, g_iH, iContinentBoundaryPlots);
+
+	AreaBuilder.Recalculate();
+
+	-- Place lakes before rivers to allow them to act as river sources
+	AddLakes();
+
+	-- River generation is affected by plot types, originating from highlands and preferring to traverse lowlands.
+	AddRivers();
+
+	AddFeatures();
+	
+	print("Adding cliffs");
+	AddCliffs(plotTypes, terrainTypes);
+	
+	local args = {
+		numberToPlace = GameInfo.Maps[Map.GetMapSize()].NumNaturalWonders,
+	};
+
+	local nwGen = NaturalWonderGenerator.Create(args);
+
+	AddFeaturesFromContinents();
+	MarkCoastalLowlands();
+	
+	local resourcesConfig = MapConfiguration.GetValue("resources");
+	local startConfig = MapConfiguration.GetValue("start");-- Get the start config
+	local args = {
+		resources = resourcesConfig,
+		iWaterLux = 4,
+		START_CONFIG = startConfig,
+	}
+	local resGen = ResourceGenerator.Create(args);
+
+	print("Creating start plot database.");
+	-- START_MIN_Y and START_MAX_Y is the percent of the map ignored for major civs' starting positions.
+	local args = {
+		MIN_MAJOR_CIV_FERTILITY = 200,
+		MIN_MINOR_CIV_FERTILITY = 50, 
+		MIN_BARBARIAN_FERTILITY = 1,
+		START_MIN_Y = 15,
+		START_MAX_Y = 15,
+		START_CONFIG = startConfig,
+	};
+	local start_plot_database = AssignStartingPlots.Create(args)
+
+	local GoodyGen = AddGoodies(g_iW, g_iH);
+end
+
+-- Input a Hash; Export width, height, and wrapX
+function GetMapInitData(MapSize)
+	local Width = 48;
+	local Height = 64;
+	local WrapX = false;
+	return {Width = Width, Height = Height, WrapX = WrapX,}
+end
+-------------------------------------------------------------------------------
+function GeneratePlotTypes(world_age)
+	print("Generating Plot Types");
+	local plotTypes = {};
+
+	-- Start with it all as water
+	for x = 0, g_iW - 1 do
+		for y = 0, g_iH - 1 do
+			local i = y * g_iW + x;
+			local pPlot = Map.GetPlotByIndex(i);
+			plotTypes[i] = g_PLOT_TYPE_OCEAN;
+			TerrainBuilder.SetTerrainType(pPlot, g_TERRAIN_TYPE_OCEAN);
+		end
+	end
+
+	-- Each land strip is defined by: Y, X Start, X End
+	local xOffset = 1;
+	local yOffset = 1;
+	local landStrips = {
+		{1, 14, 15},
+		{2, 13, 15},
+		{3, 13, 16},
+		{4, 10, 16},
+		{5, 9, 16},
+		{6, 9, 16},
+		{7, 8, 16},
+		{8, 7, 17},
+		{9, 7, 18},
+		{10, 6, 18},
+		{11, 6, 18},
+		{12, 6, 19},
+		{13, 6, 19},
+		{14, 6, 19},
+		{15, 6, 22},
+		{15, 39, 42},
+		{16, 5, 23},
+		{16, 38, 43},
+		{17, 6, 24},
+		{17, 38, 44},
+		{18, 5, 25},
+		{18, 37, 44},
+		{19, 5, 25},
+		{19, 38, 45},
+		{20, 5, 25},
+		{20, 39, 45},
+		{21, 6, 26},
+		{21, 40, 45},
+		{22, 6, 27},
+		{22, 42, 44},
+		{23, 7, 29},
+		{23, 42, 43},
+		{24, 8, 30},
+		{25, 6, 31},
+		{26, 5, 32},
+		{27, 6, 32},
+		{28, 6, 33},
+		{29, 7, 33},
+		{30, 7, 33},
+		{31, 6, 33},
+		{32, 6, 33},
+		{33, 7, 32},
+		{34, 6, 32},
+		{35, 6, 32},
+		{36, 7, 31},
+		{37, 6, 31},
+		{38, 6, 32},
+		{39, 6, 31},
+		{40, 6, 32},
+		{41, 6, 32},
+		{42, 6, 32},
+		{43, 5, 31},
+		{44, 2, 30},
+		{45, 1, 30},
+		{46, 1, 30},
+		{47, 1, 29},
+		{48, 1, 28},
+		{49, 2, 29},
+		{50, 1, 28},
+		{51, 2, 28},
+		{52, 4, 28},
+		{53, 5, 27},
+		{54, 6, 26},
+		{55, 7, 26},
+		{56, 9, 26},
+		{57, 10, 26},
+		{58, 13, 22},
+		{58, 25, 26},
+		{59, 15, 22},
+		{60, 18, 20}};
+
+		
+	for i, v in ipairs(landStrips) do
+		local y = v[1] + yOffset; 
+		local xStart = v[2] + xOffset;
+		local xEnd = v[3] + xOffset; 
+		for x = xStart, xEnd do
+			local i = y * g_iW + x;
+			local pPlot = Map.GetPlotByIndex(i);
+			plotTypes[i] = g_PLOT_TYPE_LAND;
+			TerrainBuilder.SetTerrainType(pPlot, g_TERRAIN_TYPE_SNOW);  -- temporary setting so can calculate areas
+			g_iNumTotalLandTiles = g_iNumTotalLandTiles + 1;
+		end
+	end
+		
+	AreaBuilder.Recalculate();
+	
+	local args = {};
+	args.world_age = world_age;
+	args.iW = g_iW;
+	args.iH = g_iH
+	args.iFlags = g_iFlags;
+	args.blendRidge = 10;
+	args.blendFract = 1;
+	args.extra_mountains = 4;
+	plotTypes = ApplyTectonics(args, plotTypes);
+
+	return plotTypes;
+end
+
+function InitFractal(args)
+
+	if(args == nil) then args = {}; end
+
+	local continent_grain = args.continent_grain or 2;
+	local rift_grain = args.rift_grain or -1; -- Default no rifts. Set grain to between 1 and 3 to add rifts. - Bob
+	local invert_heights = args.invert_heights or false;
+	local polar = args.polar or true;
+	local ridge_flags = args.ridge_flags or g_iFlags;
+
+	local fracFlags = {};
+	
+	if(invert_heights) then
+		fracFlags.FRAC_INVERT_HEIGHTS = true;
+	end
+	
+	if(polar) then
+		fracFlags.FRAC_POLAR = true;
+	end
+	
+	if(rift_grain > 0 and rift_grain < 4) then
+		local riftsFrac = Fractal.Create(g_iW, g_iH, rift_grain, {}, 6, 5);
+		g_continentsFrac = Fractal.CreateRifts(g_iW, g_iH, continent_grain, fracFlags, riftsFrac, 6, 5);
+	else
+		g_continentsFrac = Fractal.Create(g_iW, g_iH, continent_grain, fracFlags, 6, 5);	
+	end
+
+	-- Use Brian's tectonics method to weave ridgelines in to the continental fractal.
+	-- Without fractal variation, the tectonics come out too regular.
+	--
+	--[[ "The principle of the RidgeBuilder code is a modified Voronoi diagram. I 
+	added some minor randomness and the slope might be a little tricky. It was 
+	intended as a 'whole world' modifier to the fractal class. You can modify 
+	the number of plates, but that is about it." ]]-- Brian Wade - May 23, 2009
+	--
+	local MapSizeTypes = {};
+	for row in GameInfo.Maps() do
+		MapSizeTypes[row.MapSizeType] = row.PlateValue;
+	end
+	local sizekey = Map.GetMapSize();
+
+	local numPlates = MapSizeTypes[sizekey] or 4
+
+	-- Blend a bit of ridge into the fractal.
+	-- This will do things like roughen the coastlines and build inland seas. - Brian
+
+	g_continentsFrac:BuildRidges(numPlates, {}, 1, 2);
+end
+
+function AddFeatures()
+	print("Adding Features");
+
+	-- Get Rainfall setting input by user.
+	local rainfall = MapConfiguration.GetValue("rainfall");
+	if rainfall == 4 then
+		rainfall = 1 + TerrainBuilder.GetRandomNumber(3, "Random Rainfall - Lua");
+	end
+	
+	local args = {rainfall = rainfall, iJunglePercent = 0, iReefPercent = 3, iIcePercent = 40}	-- no rainforest
+	featuregen = FeatureGenerator.Create(args);
+
+	featuregen:AddFeatures(true, true);  --second parameter is whether or not rivers start inland);
+end
+
+------------------------------------------------------------------------------
+function GenerateTerrainTypesGreenland(plotTypes, iW, iH, iFlags, bNoCoastalMountains)
+	print("Generating Terrain Types");
+	local terrainTypes = {};
+
+	local fracXExp = -1;
+	local fracYExp = -1;
+	local grain_amount = 3;
+
+	snow = Fractal.Create(iW, iH, 
+									grain_amount, iFlags, 
+									fracXExp, fracYExp);
+									
+	iSnowTop = snow:GetHeight(85);
+
+	tundra = Fractal.Create(iW, iH, 
+									grain_amount, iFlags, 
+									fracXExp, fracYExp);
+																		
+	iTundraTop = tundra:GetHeight(100);
+	iTundraBottom = tundra:GetHeight(35);
+
+	plains = Fractal.Create(iW, iH, 
+									grain_amount, iFlags, 
+									fracXExp, fracYExp);
+																		
+	iPlainsTop = plains:GetHeight(90);
+	iPlainsBottom = plains:GetHeight(45);
+
+	grass = Fractal.Create(iW, iH, 
+									grain_amount, iFlags, 
+									fracXExp, fracYExp);
+																		
+	iGrassTop = plains:GetHeight(95);
+	iGrassBottom = plains:GetHeight(35);
+
+	for iX = 0, iW - 1 do
+		for iY = 0, iH - 1 do
+			local index = (iY * iW) + iX;
+			if (plotTypes[index] == g_PLOT_TYPE_OCEAN) then
+				if (IsAdjacentToLand(plotTypes, iX, iY)) then
+					terrainTypes[index] = g_TERRAIN_TYPE_COAST;
+				else
+					terrainTypes[index] = g_TERRAIN_TYPE_OCEAN;
+				end
+			end
+		end
+	end
+
+	if (bNoCoastalMountains == true) then
+		plotTypes = RemoveCoastalMountains(plotTypes, terrainTypes);
+	end
+
+	for iX = 0, iW - 1 do
+		for iY = 0, iH - 1 do
+			local index = (iY * iW) + iX;
+
+			local lat = (iY - iH/2)/(iH/2);
+			local long = (iX - iW/2)/(iW/2);
+
+			local iDistanceFromCenter = Map.GetPlotDistance (iX, iY, g_CenterX, g_CenterY);
+
+			local snowVal = snow:GetHeight(iX, iY);
+			local tundraVal = tundra:GetHeight(iX, iY);
+			local plainsVal = plains:GetHeight(iX, iY);
+			local grassVal = grass:GetHeight(iX, iY);
+
+			-- Iceland
+			if (long > 0.5 and lat < -0.25) then
+				local iSnowBottom = snow:GetHeight(80);
+
+				if (plotTypes[index] == g_PLOT_TYPE_MOUNTAIN) then
+					terrainTypes[index] = g_TERRAIN_TYPE_TUNDRA_MOUNTAIN;
+
+					if ((grassVal >= iGrassBottom) and (grassVal <= iGrassTop)) then
+						terrainTypes[index] = g_TERRAIN_TYPE_GRASS_MOUNTAIN;
+					elseif ((plainsVal >= iPlainsBottom) and (plainsVal <= iPlainsTop)) then
+						terrainTypes[index] = g_TERRAIN_TYPE_PLAINS_MOUNTAIN;
+					elseif ((snowVal >= iSnowBottom) and (snowVal <= iSnowTop)) then
+						terrainTypes[index] = g_TERRAIN_TYPE_SNOW_MOUNTAIN;
+					end
+
+				elseif (plotTypes[index] ~= g_PLOT_TYPE_OCEAN) then
+					terrainTypes[index] = g_TERRAIN_TYPE_TUNDRA;
+
+					if ((grassVal >= iGrassBottom) and (grassVal <= iGrassTop)) then
+						terrainTypes[index] = g_TERRAIN_TYPE_GRASS;
+					elseif ((plainsVal >= iPlainsBottom) and (plainsVal <= iPlainsTop)) then
+						terrainTypes[index] = g_TERRAIN_TYPE_PLAINS;
+					elseif ((snowVal >= iSnowBottom) and (snowVal <= iSnowTop)) then
+						terrainTypes[index] = g_TERRAIN_TYPE_SNOW;
+					end
+				end
+
+			-- Greenland
+			else
+				local iSnowBottom = snow:GetHeight(iDistanceFromCenter/iH * 100);
+
+				if (plotTypes[index] == g_PLOT_TYPE_MOUNTAIN) then
+					terrainTypes[index] = g_TERRAIN_TYPE_GRASS_MOUNTAIN;
+
+					if ((snowVal >= iSnowBottom) and (snowVal <= iSnowTop)) then
+						terrainTypes[index] = g_TERRAIN_TYPE_SNOW_MOUNTAIN;
+					elseif ((tundraVal >= iTundraBottom) and (tundraVal <= iTundraTop)) then
+						terrainTypes[index] = g_TERRAIN_TYPE_TUNDRA_MOUNTAIN;
+					elseif ((plainsVal >= iPlainsBottom) and (plainsVal <= iPlainsTop)) then
+						terrainTypes[index] = g_TERRAIN_TYPE_PLAINS_MOUNTAIN;
+					end
+
+				elseif (plotTypes[index] ~= g_PLOT_TYPE_OCEAN) then
+					terrainTypes[index] = g_TERRAIN_TYPE_GRASS;
+				
+					if ((snowVal >= iSnowBottom) and (snowVal <= iSnowTop)) then
+						terrainTypes[index] = g_TERRAIN_TYPE_SNOW;
+					elseif ((tundraVal >= iTundraBottom) and (tundraVal <= iTundraTop)) then
+						terrainTypes[index] = g_TERRAIN_TYPE_TUNDRA;
+					elseif ((plainsVal >= iPlainsBottom) and (plainsVal <= iPlainsTop)) then
+						terrainTypes[index] = g_TERRAIN_TYPE_PLAINS;
+					end
+				end
+			end
+		end
+	end
+
+	local bExpandCoasts = true;
+
+	if bExpandCoasts == false then
+		return
+	end
+
+	print("Expanding coasts");
+	for iI = 0, 2 do
+		local shallowWaterPlots = {};
+		for iX = 0, iW - 1 do
+			for iY = 0, iH - 1 do
+				local index = (iY * iW) + iX;
+				if (terrainTypes[index] == g_TERRAIN_TYPE_OCEAN) then
+					-- Chance for each eligible plot to become an expansion is 1 / iExpansionDiceroll.
+					-- Default is two passes at 1/4 chance per eligible plot on each pass.
+					if (IsAdjacentToShallowWater(terrainTypes, iX, iY) and TerrainBuilder.GetRandomNumber(4, "add shallows") == 0) then
+						table.insert(shallowWaterPlots, index);
+					end
+				end
+			end
+		end
+		for i, index in ipairs(shallowWaterPlots) do
+			terrainTypes[index] = g_TERRAIN_TYPE_COAST;
+		end
+	end
+	
+	return terrainTypes; 
+end
+
+------------------------------------------------------------------------------
+function FeatureGenerator:AddIceToMap()
+	local iTargetIceTiles = (self.iGridH * self.iGridW *  (GlobalParameters.ICE_TILES_PERCENT + self.iIceModifiedPercent)) / 100;
+
+	local aPhases = {};
+	local iPhases = 0;
+	for row in GameInfo.RandomEvents() do
+		if (row.EffectOperatorType == "SEA_LEVEL") then
+			local kPhaseDetails = {};
+			kPhaseDetails.RandomEventEnum = row.Index;
+			kPhaseDetails.IceLoss = row.IceLoss;
+			table.insert(aPhases, kPhaseDetails);
+			iPhases = iPhases + 1;
+		end
+	end
+	
+	if (iPhases <= 0) then 
+		return;
+	end
+
+	------------------------------
+	-- PHASE ONE: PERMANENT ICE --
+	------------------------------
+	local iIceLossThisLevel = aPhases[iPhases].IceLoss;
+	local iPermanentIcePercent = 100 - iIceLossThisLevel;
+	local iPermanentIceTiles = (iTargetIceTiles * iPermanentIcePercent) / 100;
+
+	print ("Permanent Ice Tiles: " .. tostring(iPermanentIceTiles));
+
+	-- Count top/bottom map tiles
+	local iWaterTilesOnEdges = 0;
+
+	--   On bottom
+	for x = 0, self.iGridW - 1, 1 do
+		y = 0;
+		local i = y * self.iGridW + x;
+		local plot = Map.GetPlotByIndex(i);
+		if (plot ~= nil) then
+			if(TerrainBuilder.CanHaveFeature(plot, g_FEATURE_ICE) == true and IsAdjacentToLandPlot(x, y) == false) then
+				iWaterTilesOnEdges = iWaterTilesOnEdges + 1;
+			end
+		end
+	end
+
+	--   On top
+	for x = 0, self.iGridW - 1, 1 do
+		local y = self.iGridH - 1;
+		local i = y * self.iGridW + x;
+		local plot = Map.GetPlotByIndex(i);
+		if (plot ~= nil) then
+			if(TerrainBuilder.CanHaveFeature(plot, g_FEATURE_ICE) == true and IsAdjacentToLandPlot(x, y) == false) then
+				iWaterTilesOnEdges = iWaterTilesOnEdges + 1;
+			end
+		end
+	end
+
+	if (iWaterTilesOnEdges > 0) then
+		local iPercentNeeded = 100 * iPermanentIceTiles / iWaterTilesOnEdges;
+
+		for x = 0, self.iGridW - 1, 1 do
+			local y = self.iGridH - 1;
+			local i = y * self.iGridW + x;
+			local plot = Map.GetPlotByIndex(i);
+			if (plot ~= nil) then
+				if(TerrainBuilder.CanHaveFeature(plot, g_FEATURE_ICE) == true and IsAdjacentToLandPlot(x, y) == false) then
+					if (TerrainBuilder.GetRandomNumber(100, "Permanent Ice") <= iPercentNeeded) then
+						TerrainBuilder.SetFeatureType(plot, g_FEATURE_ICE);
+						TerrainBuilder.AddIce(plot:GetIndex(), -1); 
+					end
+				end
+			end
+		end
+	end
+
+	---------------------------------------
+	-- PHASE TWO: ICE THAT CAN DISAPPEAR --
+	---------------------------------------
+	if (iPhases > 1) then
+		for iPhaseIndex = iPhases, 1, -1 do
+			kPhaseDetails = aPhases[iPhaseIndex];
+			local iIcePercentToAdd = 0;
+			if (iPhaseIndex == 1) then 
+				iIcePercentToAdd = kPhaseDetails.IceLoss;			
+			else
+				iIcePercentToAdd = kPhaseDetails.IceLoss - aPhases[iPhaseIndex - 1].IceLoss;
+			end
+			local iIceTilesToAdd = (iTargetIceTiles * iIcePercentToAdd) / 100;
+
+			print ("iPhaseIndex: " .. tostring(iPhaseIndex) .. ", iIceTilesToAdd: " .. tostring(iIceTilesToAdd) .. ", RandomEventEnum: " .. tostring(kPhaseDetails.RandomEventEnum));
+
+			-- Find all plots on map adjacent to already-placed ice
+			local aTargetPlots = {};
+			for y = 0, self.iGridH - 1, 1 do
+				for x = 0, self.iGridW - 1, 1 do
+					local i = y * self.iGridW + x;
+					local plot = Map.GetPlotByIndex(i);
+					if (plot ~= nil) then
+						local iAdjacent = TerrainBuilder.GetAdjacentFeatureCount(plot, g_FEATURE_ICE);
+						if (TerrainBuilder.CanHaveFeature(plot, g_FEATURE_ICE) == true and iAdjacent > 0) then
+							local kPlotDetails = {};
+							kPlotDetails.PlotIndex = i;
+							kPlotDetails.AdjacentIce = iAdjacent;
+							kPlotDetails.AdjacentToLand = IsAdjacentToLandPlot(x, y);
+							table.insert(aTargetPlots, kPlotDetails);
+						end
+					end
+				end
+			end
+
+			-- Roll die to see which of these get ice
+			if (#aTargetPlots > 0) then
+				local iPercentNeeded = 100 * iIceTilesToAdd / #aTargetPlots;
+				for i, targetPlot in ipairs(aTargetPlots) do
+					local iFinalPercentNeeded = iPercentNeeded + 10 * targetPlot.AdjacentIce;
+					if (targetPlot.AdjacentToLand == true) then
+						iFinalPercentNeeded = iFinalPercentNeeded / 5;
+					end
+					if (TerrainBuilder.GetRandomNumber(100, "Permanent Ice") <= iFinalPercentNeeded) then
+					    local plot = Map.GetPlotByIndex(targetPlot.PlotIndex);
+						TerrainBuilder.SetFeatureType(plot, g_FEATURE_ICE);
+						TerrainBuilder.AddIce(plot:GetIndex(), kPhaseDetails.RandomEventEnum); 
+					end
+				end
+			end
+		end
+	end
+end
+
+------------------------------------------------------------------------------
+function AddFeaturesFromContinents()
+	print("Adding Features from Continents");
+
+	featuregen:AddFeaturesFromContinents();
+end
